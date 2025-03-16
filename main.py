a993b2423447bfb4f389b65c1af2e68f90df3180
bosen-you <samuelsamuelsamuel123454321@gmail.com>
2025-03-16 22:17:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import *
 import os
-from scipy.io import wavfile 
+from scipy.io import wavfile
 from machine import Pin,SoftI2C
 import ssd1306  
 
@@ -11,14 +11,13 @@ if __name__ == '__main__':
     try:
         action = int(input("輸入數字就好了\n1. transform\n2. compare\n3. encode\n4. decode\n"))
         if action == 1:
-            # ssd1306
             lcd.fill(0)
-            lcd.text("transform",20,52)       #set "" at (0,0)
+            lcd.text("transform",0,0)       #set "" at (0,0)
             lcd.show()#display
-            # 
-            rate, audio, desktop_path = select_audio_file()
-            waveform_img_path = os.path.join(desktop_path, "waveform.png")
-            spectrum_img_path = os.path.join(desktop_path, "spectrum.png")
+
+            rate, audio, path = select_audio_file()
+            waveform_img_path = os.path.join(path, "waveform.png")
+            spectrum_img_path = os.path.join(path, "spectrum.png")
 
             # 繪製並顯示+儲存波形圖
             plot_waveform(audio, audio, rate, "Waveform Watermark", "encode", save_path=waveform_img_path)
@@ -28,20 +27,20 @@ if __name__ == '__main__':
             plot_frequency_spectrum(audio, rate, "Original Audio", save_path=spectrum_img_path)
             print(f"頻譜圖已保存至: {spectrum_img_path}")
         elif action == 2:
-            # ssd1306
             lcd.fill(0)
-            lcd.text("compare",20,52)       #set "" at (0,0)
+            lcd.text("compare",0,0)       #set "" at (0,0)
             lcd.show()#display
-
-            #
-            rate1, audio1, desktop_path = select_audio_file()
-            rate2, audio2, _ = select_audio_file()
+            
+            path1 = select_audio_file_path()
+            path2 = select_audio_file_path()
 
             cho = int(input('輸入數字就好了\n1. 顯示數據比較\n2. 顯示圖片比較\n'))
             if cho == 1:
-                final = analyze_audio_similarity(rate1, audio1, rate2, audio2)
-                l = ['0% ~ 20%\nRisk-free', '20% ~ 40%\nSecure', '40% ~ 60%\nModerate', '60% ~ 80%\nDangerous', '80% ~ 100%\nHazardous']
-                rate_check = [[0, 20], [20, 40], [40, 60], [60, 80], [80, 101]]
+                lcd.text("data analysis", 20, 52)
+
+                final = compare_audio(path1, path2)
+                l = ['0% ~ 0%\nRisk-free', '20% ~ 40%\nSecure', '40% ~ 60%\nModerate', '60% ~ 80%\nDangerous', '80% ~ 100%\nHazardous']
+                rate_check = [[0, 0], [20, 40], [40, 60], [60, 80], [80, 101]]
                 
                 for i in range(5):
                     if rate_check[i][1] > final >= rate_check[i][0]:
@@ -49,37 +48,41 @@ if __name__ == '__main__':
                         break
                     
             elif cho == 2:
-                plot_audio_comparison(rate1, audio1, desktop_path, audio2)
+                lcd.text("graph analysis", 20, 52)
+                chose = input('輸入數字就好了\n1. 波型圖\n2. 頻譜圖\n')
+                if chose == '1':
+                    plot_waveform(path1, path2)
+                elif chose == '2':
+                    plot_spectrogram(path1, path2)
+                else:
+                    print("輸入格式錯誤")
+                    exit(1)
             else: 
                 print("輸入格式錯誤")
                 exit(1)
             
         elif action == 3:
-            # ssd1306
             lcd.fill(0)
-            lcd.text("encode",20,52)       #set "" at (0,0)
+            lcd.text("encode",0,0)       #set "" at (0,0)
             lcd.show()#display
 
-            #
-            rate, audio, desktop_path = select_audio_file()
+            rate, audio, path = select_audio_file()
 
             encryption_id = generate_encryption_id()
             accelerated_audio = speed_up_audio(audio, rate, 2.0)
             encoded_audio = embed_watermark(accelerated_audio, encryption_id)
 
             output_name = input("請輸入加密後檔案的名稱: ")
-            output_path = os.path.join(desktop_path, f"{output_name}_{encryption_id}.wav")
+            output_path = os.path.join(path, f"{output_name}_{encryption_id}.wav")
             wavfile.write(output_path, rate, encoded_audio)
             print(f"嵌入完成，結果已保存至: {output_path}")
            
         elif action == 4:
-            # ssd1306
             lcd.fill(0)
-            lcd.text("decode",20,52)       #set "" at (0,0)
+            lcd.text("decode",0 ,0)       #set "" at (0,0)
             lcd.show()#display
 
-            #
-            rate, audio, desktop_path = select_audio_file()
+            rate, audio, path = select_audio_file()
             extracted_id = extract_watermark(audio)
             psw = input("請輸入密鑰進行驗證: ")
 
@@ -87,7 +90,7 @@ if __name__ == '__main__':
                 remove = input("是否移除水印並生成解密後音檔？(y/n): ").lower() == 'y'
                 if remove:
                     output_name = input("請輸入解密後檔案的名稱: ")
-                    output_path = os.path.join(desktop_path, f"{output_name}_watermark_removed.wav")
+                    output_path = os.path.join(path, f"{output_name}_watermark_removed.wav")
                     wavfile.write(output_path, rate, audio)
                     print(f"水印已移除，解密後的音頻已保存至: {output_path}")
 
