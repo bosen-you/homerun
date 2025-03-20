258c4e278248acca3d04b14fbf2948d8fa3f248d
bosen-you <samuelsamuelsamuel123454321@gmail.com>
2025-03-20 13:50:38 +0800

    Update decode.py

--- utils/decode.py
+++ utils/decode.py
@@ -1,11 +1,12 @@
-# 提取加密標識
+# extract watermark
 def extract_watermark(audio, watermark_length=32):
     audio = audio.flatten()
+    # LSB
     watermark_bits = ''.join(str(audio[i] & 1) for i in range(watermark_length))
     encryption_id = ''.join(hex(int(watermark_bits[i:i + 4], 2))[2:].upper() for i in range(0, len(watermark_bits), 4))
     return encryption_id
 
-# 移除加密標識
+# remove watermark
 def remove_watermark(audio, watermark_length=32):
     audio = audio.copy().flatten()
     for i in range(watermark_length):
